5479e70a625446fce4ed9dcd0d5409d243799b4f
Evelio Tarazona <evelio@evelio.info>
2013-07-13 21:47:23 -0700

    Catchup with checkstyle add header to java files

--- carbonite/src/main/java/info/evelio/carbonite/BaseOptions.java
+++ carbonite/src/main/java/info/evelio/carbonite/BaseOptions.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Evelio Tarazona Cáceres <evelio@evelio.info>
+ * Copyright 2013 Carbonite contributors <contributors@evelio.info>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package info.evelio.carbonite;
 
 import android.content.Context;
@@ -11,6 +27,7 @@ import static info.evelio.carbonite.Carbonite.Defaults.LOAD_FACTOR;
 import static info.evelio.carbonite.Carbonite.Defaults.TYPE;
 import static info.evelio.carbonite.util.Util.illegalArg;
 import static info.evelio.carbonite.util.Util.notNullArg;
+
 
 public class BaseOptions implements CarboniteBuilder.Options {
   private final CarboniteBuilder mCarboniteBuilder;
--- carbonite/src/main/java/info/evelio/carbonite/util/Util.java
+++ carbonite/src/main/java/info/evelio/carbonite/util/Util.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Evelio Tarazona Cáceres <evelio@evelio.info>
+ * Copyright 2013 Carbonite contributors <contributors@evelio.info>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package info.evelio.carbonite.util;
 
 import info.evelio.carbonite.future.Present;
@@ -22,8 +38,11 @@ import java.util.regex.Pattern;
  *
  * @hide
  */
-public class Util {
+public final class Util {
 
+  private Util() {
+    // No instances baby!
+  }
 
   public static int len(Object[] arr) {
     return empty(arr) ? 0 : arr.length;
